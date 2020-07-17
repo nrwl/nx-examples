bfbdeaf4b22551ded44da12d9417feca5ea4ab8c
Jason Jean <jasonjean1993@gmail.com>
2020-07-17 14:24:43 -0400

    update to nx 10

--- apps/cart/jest.config.js
+++ apps/cart/jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
   name: 'cart',
   preset: '../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/cart'
+  coverageDirectory: '../../coverage/apps/cart',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
