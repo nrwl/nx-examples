a5371372307a11f299cee82f755b5d0bf45f5052
Jason Jean <jasonjean1993@gmail.com>
2020-07-31 16:06:39 -0400

    update to nx 10 (#110)

--- libs/shared/header/jest.config.js
+++ libs/shared/header/jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
   name: 'shared-header',
   preset: '../../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/shared/header'
+  coverageDirectory: '../../../coverage/libs/shared/header',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
--- libs/shared/product/ui/jest.config.js
+++ libs/shared/product/ui/jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
   name: 'shared-product-ui',
   preset: '../../../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../../coverage/libs/shared/product/ui'
+  coverageDirectory: '../../../../coverage/libs/shared/product/ui',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
