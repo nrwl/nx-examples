d3c29ba523c51713c65e1d4b58eada9a84d39bf6
Joep Kockelkorn <joepkockelkorn@gmail.com>
2020-11-11 17:26:02 +0100

    format

--- apps/products/src/app/app.component.ts
+++ apps/products/src/app/app.component.ts
@@ -5,6 +5,6 @@ import '@nx-example/shared/header-renamed';
 @Component({
   selector: 'nx-example-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
