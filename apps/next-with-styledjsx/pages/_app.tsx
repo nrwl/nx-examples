fe9bc37bf162e4f9c053282d7691fe225604ee9c
antoniojps <antonio.santos@mindera.com>
2021-02-20 00:35:36 +0000

    feat(next): add next-with-styled-jsx example

---
+++ apps/next-with-styledjsx/pages/_app.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+import './styles.css';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to next-with-styledjsx!</title>
+      </Head>
+      <div className="app">
+        <header className="flex">
+          <NxLogo width="75" height="50" />
+          <h1>Welcome to next-with-styledjsx!</h1>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
