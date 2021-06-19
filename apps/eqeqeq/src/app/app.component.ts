15b9c384cf6bd73b385e1d449ab1d98d8345ffe6
jeff.schulz@gubo.com <jeffschulz@JEFFs-MacBook-Pro.local>
2021-06-18 17:54:15 -0700

    eqeqeq app wirh .eslintrc.json rule

---
+++ apps/eqeqeq/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  ready = true;
+  title = 'eqeqeq';
+}
