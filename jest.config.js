01bf8d48eeaa80885588393b25e3ee07ec491594
Jason Jean <jasonjean1993@gmail.com>
2021-07-15 19:29:29 -0400

    update nx to 12.6.0-beta.7 (#158)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/products',
-    '<rootDir>/apps/cart',
-    '<rootDir>/libs/shared/product/ui',
-    '<rootDir>/libs/products/home-page',
-    '<rootDir>/libs/cart/cart-page',
-    '<rootDir>/libs/shared/cart/state',
-    '<rootDir>/libs/shared/product/state',
-    '<rootDir>/libs/shared/header',
-    '<rootDir>/libs/products/product-detail-page',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
