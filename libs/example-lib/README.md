9b2ec7e4f7d11e925627cf0a4724ee7e1f246a15
Morris Janatzek <morrisj@live.de>
2021-12-08 17:10:24 +0100

    added basic reproduction for 'enforce-module-boundaries' es-lint rule error in angular secondary entrypoints library

---
+++ libs/example-lib/README.md
@@ -0,0 +1,7 @@
+# example-lib
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test example-lib` to execute the unit tests.
---
+++ libs/example-lib/models/implementations/src/lib/example-model.ts
@@ -0,0 +1,5 @@
+import {ExampleModel as IExampleModel} from '@example/lib/models/abstract';
+
+export class ExampleModel implements IExampleModel {
+  someField!: string;
+}
