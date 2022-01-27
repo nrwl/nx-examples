1d2a1da4c2e1400f70f0c5d09f8805082dfe9a2e
Shane Glass <shane@shattered.dev>
2022-01-27 13:17:02 +1000

    INIT

---
+++ apps/cart/src/app/test-storybook-success/test-storybook-success.tsx
@@ -0,0 +1,14 @@
+import './test-storybook-success.module.css';
+
+/* eslint-disable-next-line */
+export interface TestStorybookSuccessProps {}
+
+export function TestStorybookSuccess(props: TestStorybookSuccessProps) {
+  return (
+    <div>
+      <h1>Welcome to TestStorybookSuccess!</h1>
+    </div>
+  );
+}
+
+export default TestStorybookSuccess;
---
+++ libs/cart/cart-page/src/lib/test-storybook/test-storybook.tsx
@@ -0,0 +1,12 @@
+import './test-storybook.module.css';
+
+/* eslint-disable-next-line */
+export interface TestStorybookProps {}
+
+export function TestStorybook(props: TestStorybookProps) {
+  return (
+    <img src="fdasf" />
+  );
+}
+
+export default TestStorybook;
