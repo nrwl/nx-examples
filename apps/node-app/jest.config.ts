5d7199ef0c5b03b83bf267f389c0cc35413fa36f
Johannes Gest <kiiu.rib@gmail.com>
2022-06-30 19:18:18 +0200

    added a node app

---
+++ apps/node-app/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'node-app',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/next-offer-api',
+  preset: '../../jest.preset.js',
+};
