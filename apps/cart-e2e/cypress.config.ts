a1c8b27adaedd5e618d8d7b25f1fe92de252b56a
Jason Jean <jasonjean1993@gmail.com>
2022-07-20 16:14:26 -0400

    chore: update cypress to v10

---
+++ apps/cart-e2e/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/cart-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/apps/cart-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: false,
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
+});
