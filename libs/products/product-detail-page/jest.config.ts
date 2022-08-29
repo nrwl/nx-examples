c0789cb2457f3d1932080dd514d5e3beb7b67663
Jason Jean <jasonjean1993@gmail.com>
2022-08-29 12:18:51 -0400

    update nx to 14.6.0-beta.9

--- libs/products/product-detail-page/jest.config.ts
+++ libs/products/product-detail-page/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default {
   coverageDirectory: '../../../coverage/libs/products/product-detail-page',
 
--- libs/shared/product/state/jest.config.ts
+++ libs/shared/product/state/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default {
   coverageDirectory: '../../../../coverage/libs/shared/product/state',
 
