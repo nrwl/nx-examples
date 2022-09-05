48ae75ccd4c16aecba93a5f4d36281547eb2e081
Stephan Muller <stephan.muller@rabobank.nl>
2022-09-05 16:07:54 +0200

    chore: add another dependency just to be sure

--- apps/cart/src/app/app.tsx
+++ apps/cart/src/app/app.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import * as transitiveDependency from '@jridgewell/gen-mapping';
+import * as transitiveDependency2 from 'ansi-colors';
 import * as peerDependency from 'fuzzy';
 
 import '@nx-example/shared/header';
@@ -22,5 +23,6 @@ export default App;
 // make sure no other linting warnings like "unused variables" are thrown, by pretending to use the imports
 export const testfunc = {
   transitiveDependency,
+  transitiveDependency2,
   peerDependency,
 };
