dc23aff3673d1c8c03c234c1a89e1c70519b69ae
Riley Brown <ribrown@bishs.com>
2022-10-21 09:30:00 -0600

    Adding a couple of possible linting errors

--- apps/cart/src/app/app.tsx
+++ apps/cart/src/app/app.tsx
@@ -3,8 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import '@nx-example/shared/header';
 
 import { CartCartPage } from '@nx-example/cart/cart-page';
+import { useEffect } from 'react';
 
 export const App = () => {
+
+  if(true) {
+		useEffect(() => {
+			console.log("test");
+		}, []);
+	}
+
   return (
     <>
       <nx-example-header />
