c6b1186baa4e484336a1fb62f8eb55e27419b86f
Michał Mrozek <mail@michalmrozek.dev>
2023-10-05 13:06:00 +0200

    bug: workspace libs resolution in webpack ts config

--- apps/cart/webpack.config.ts
+++ apps/cart/webpack.config.ts
@@ -1,15 +1,17 @@
-const { composePlugins, withNx } = require('@nx/webpack');
-const { withReact } = require('@nx/react');
+import { composePlugins, withNx } from '@nx/webpack';
+import { withReact } from '@nx/react';
+import { products } from '@nx-example/shared/product/data';
 
 // Nx plugins for webpack.
-module.exports = composePlugins(
+export default composePlugins(
   withNx(),
-  withReact(),
+  withReact() as any,
   (config, { options, context }) => {
     // Update the webpack config as needed here.
     // e.g. config.plugins.push(new MyPlugin())
     // For more information on webpack config and Nx see:
     // https://nx.dev/packages/webpack/documents/webpack-config-setup
+    console.log('products', products);
     return config;
   }
 );
