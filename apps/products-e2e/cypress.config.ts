4a3a004e84bade068c2ffa974ea06617fee273a7
FrozenPandaz <jasonjean1993@gmail.com>
2024-02-03 21:31:05 -0500

    chore(repo): make crystal clear

--- apps/products-e2e/cypress.config.ts
+++ apps/products-e2e/cypress.config.ts
@@ -1,26 +1,26 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
-const cypressJsonConfig = {
-  fileServerFolder: '.',
-  fixturesFolder: './src/fixtures',
-  video: true,
-  videosFolder: '../../dist/cypress/apps/products-e2e/videos',
-  screenshotsFolder: '../../dist/cypress/apps/products-e2e/screenshots',
-  chromeWebSecurity: false,
-  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  supportFile: false,
-};
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename),
-    ...cypressJsonConfig,
-    /**
-     * TODO(@nx/cypress): In Cypress v12,the testIsolation option is turned on by default.
-     * This can cause tests to start breaking where not indended.
-     * You should consider enabling this once you verify tests do not depend on each other
-     * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
-     **/
+    ...nxE2EPreset(__filename, {
+      webServerCommands: {
+        default: 'nx serve products',
+      },
+      ciWebServerCommand: 'nx run products:serve',
+      webServerConfig: {
+        timeout: 30000,
+      },
+    }),
+    baseUrl: 'http://localhost:4200',
+    fileServerFolder: '.',
+    fixturesFolder: './src/fixtures',
+    video: true,
+    videosFolder: '../../dist/cypress/apps/products-e2e/videos',
+    screenshotsFolder: '../../dist/cypress/apps/products-e2e/screenshots',
+    chromeWebSecurity: false,
+    specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
     testIsolation: false,
   },
 });
