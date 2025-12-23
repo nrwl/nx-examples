1aaa8e99c2ba0935e60ad3b2b5f2d4938dcbcc5a
Qing Lin <qing.lin@skyscanner.net>
2025-12-23 16:20:19 +0000

    Use npm workspaces instead of TS paths aliases

--- apps/cart/src/app/app.tsx
+++ apps/cart/src/app/app.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import '@nx-example/shared/header';
 
-import { CartCartPage } from '@nx-example/cart/cart-page';
+import { CartCartPage } from '@nx-example/cart-cart-page/src/lib/cart-cart-page/cart-cart-page';
 
 export const App = () => {
   return (
