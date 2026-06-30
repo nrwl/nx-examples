aaeb4882b8cb55b6ac2ef2ceea711865be1b9afa
FrozenPandaz <jasonjean1993@gmail.com>
2026-06-30 18:28:29 -0400

    chore(repo): [nx migration] apply nx 23.1.0-beta.5 migrations

--- apps/products/src/app/app.component.ts
+++ apps/products/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import '@nx-example/shared-header';
 
@@ -6,6 +6,7 @@ import '@nx-example/shared-header';
   selector: 'nx-example-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
   standalone: false,
 })
 export class AppComponent {}
