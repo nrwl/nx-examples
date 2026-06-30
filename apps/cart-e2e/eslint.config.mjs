95896f69eeb7aa366e52f082d48fab8099f795f7
FrozenPandaz <jasonjean1993@gmail.com>
2026-06-30 18:49:26 -0400

    chore(repo): format flat-config migration output with prettier

--- apps/cart-e2e/eslint.config.mjs
+++ apps/cart-e2e/eslint.config.mjs
@@ -1,46 +1,35 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
-import baseConfig from "../../eslint.config.mjs";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import baseConfig from '../../eslint.config.mjs';
 
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
 });
 
-
 export default [
-    ...baseConfig,
-    ...compat.extends("plugin:cypress/recommended"),
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.js",
-            "**/*.jsx"
-        ],
-        // Override or add rules here
-        rules: {},
-        languageOptions: {
-            parserOptions: {
-                project: "apps/cart-e2e/tsconfig.*?.json"
-            }
-        }
+  ...baseConfig,
+  ...compat.extends('plugin:cypress/recommended'),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parserOptions: {
+        project: 'apps/cart-e2e/tsconfig.*?.json',
+      },
     },
-    {
-        files: [
-            "src/plugins/index.js"
-        ],
-        rules: {
-            "@typescript-eslint/no-var-requires": "off",
-            "no-undef": "off"
-        }
+  },
+  {
+    files: ['src/plugins/index.js'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-undef': 'off',
     },
-    {
-        ignores: [
-            "out-tsc",
-            "src/test-setup.ts"
-        ]
-    }
+  },
+  {
+    ignores: ['out-tsc', 'src/test-setup.ts'],
+  },
 ];
--- libs/shared/e2e-utils/eslint.config.mjs
+++ libs/shared/e2e-utils/eslint.config.mjs
@@ -1,63 +1,53 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
-import baseConfig from "../../../eslint.config.mjs";
-import nx from "@nx/eslint-plugin";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import baseConfig from '../../../eslint.config.mjs';
+import nx from '@nx/eslint-plugin';
 
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
 });
 
-
 export default [
-    ...baseConfig,
-    ...nx.configs["flat/angular"],
-    ...compat.config({
-        extends: [
-            "plugin:@angular-eslint/template/process-inline-templates"
+  ...baseConfig,
+  ...nx.configs['flat/angular'],
+  ...compat
+    .config({
+      extends: ['plugin:@angular-eslint/template/process-inline-templates'],
+      plugins: ['@typescript-eslint'],
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.ts'],
+      rules: {
+        ...config.rules,
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
         ],
-        plugins: [
-            "@typescript-eslint"
-        ]
-    }).map(config => ({
-        ...config,
-        files: [
-            "**/*.ts"
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
         ],
-        rules: {
-            ...config.rules,
-            "@angular-eslint/directive-selector": [
-                "error",
-                {
-                    type: "attribute",
-                    prefix: "app",
-                    style: "camelCase"
-                }
-            ],
-            "@angular-eslint/component-selector": [
-                "error",
-                {
-                    type: "element",
-                    prefix: "app",
-                    style: "kebab-case"
-                }
-            ]
+      },
+      languageOptions: {
+        parserOptions: {
+          project: ['libs/shared/e2e-utils/tsconfig.*?.json'],
         },
-        languageOptions: {
-            parserOptions: {
-                project: [
-                    "libs/shared/e2e-utils/tsconfig.*?.json"
-                ]
-            }
-        }
+      },
     })),
-    ...nx.configs["flat/angular-template"],
-    {
-        ignores: [
-            "out-tsc",
-            "src/test-setup.ts"
-        ]
-    }
+  ...nx.configs['flat/angular-template'],
+  {
+    ignores: ['out-tsc', 'src/test-setup.ts'],
+  },
 ];
